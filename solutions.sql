660579aac4beb7b7cb5e088a57d3d703ec59dcbf
Maduvha734 <0407220606088@tpc.local>
2025-11-11 15:44:25 +0200

    updated readme.md,solutions.sql

---
+++ solutions.sql
@@ -0,0 +1,14 @@
+-- Question 1: Write a query to select all employees who work in the 'HR' department.
+-- This query filters and displays all employees who belong to the HR department.
+
+
+-- Question 2: Write a query to select all employees with a salary greater than $70,000.
+-- This query returns all employees whose salary is more than $80,000.
+
+
+
+-- Question 3: Write a query to select all employees hired after January 1st, 2019.
+-- This query shows employees hired after the specified date (excluding January 1st, 2019).
+
+-- Question 4: Write a query to delete all employees who work in the 'Sale' department.
+-- This query permanently removes all employees from the Sale department from the database.
